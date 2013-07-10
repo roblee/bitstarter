d15b583a7639c953ead2b2dd6b6064e9630124e3
iq 42 <robbie9@gmail.com>
2013-07-10 07:50:02 +0000

    Updating web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile("index.html", 'utf8', function(err, data) {
+    if (err) {
+      response.send('ERROR: ' + err);
+    } else {
+      response.send(data);
+    }
+  }
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
