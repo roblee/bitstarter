50fc6a0b9465a9b11fb5e25c72c38986a19229d9
iq 42 <robbie9@gmail.com>
2013-07-10 08:04:39 +0000

    Updating web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var readableStream = fs.createReadStream("index.html", {flags: 'r', encoding: 'utf8'});
+  var readStream = fs.createReadStream("index.html", {flags: 'r', encoding: 'utf8'});
   //var buf = new Buffer("hello world from buffer");
-  var buf = readableStream.read(); 
-  response.send(buf.toString());
+  //var buf = readStream.read(); 
+  response.send(readStream);
 });
 
 var port = process.env.PORT || 5000;
